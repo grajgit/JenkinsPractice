2700c03ed539dbf4b64a6033c78263d677f416fd
grajgit <rajeshudt12@gmail.com>
2023-03-12 01:26:55 +0530

    new rep

---
+++ src/org/vrtry/Empdet.java
@@ -0,0 +1,48 @@
+package org.vrtry;
+
+public class Empdet {
+
+	public void empage() {
+		// TODO Auto-generated method stub
+byte age = 25;
+
+System.out.println("employee age is "+age);
+
+	}
+	public static void main(String[] args) {
+		Empdet c = new Empdet();
+		
+		c.empage();
+	
+		short bikeNum = 1234;
+		System.out.println("emp bike num is "+bikeNum);
+		
+		short bikeNum1 = 1234;
+		System.out.println("emp bike num is "+bikeNum1);
+		
+		int pincode = 642126;
+		System.out.println("emp pincode is "+pincode);
+		
+		int pincode1 = 642126;
+		System.out.println("emp pincode is "+pincode1);
+		
+		int pincode2 = 642126;
+		System.out.println("emp pincode is "+pincode2);
+		
+		long phNum = 9790373780l;
+		System.out.println("emp contact num is "+phNum);
+		
+		char grade = 'A';
+		System.out.println("emp grade is "+grade);
+		
+		String name = "Rajesh";
+		System.out.println("employee name is "+name);
+
+		float height = 154.55f;
+		System.out.println("emp height is "+height);
+		
+		double share = 1234.567890;
+		System.out.println("emp share is "+share);
+		
+	}
+}
